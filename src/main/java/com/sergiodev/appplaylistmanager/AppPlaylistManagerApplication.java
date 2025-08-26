d32c7b41156a02787ddcc62ee71a72daa038af99
Sergio Garcia <sergio.garcia@sofka.com.co>
2025-08-26 16:44:21 -0500

    Initial commit

---
+++ src/main/java/com/sergiodev/appplaylistmanager/AppPlaylistManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sergiodev.appplaylistmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPlaylistManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPlaylistManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergiodev/appplaylistmanager/AppPlaylistManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiodev.appplaylistmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPlaylistManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
